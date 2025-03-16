4701bc7192d13973478570a562bbe0009b69fb39
Sherry <sherry.y.matias@gmail.com>
2025-03-16 21:14:46 +0200

    Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -21,7 +21,7 @@ java -version
 openjdk version "17.0.13" 2024-10-15
 OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
 OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
-
+sudo snap install openjdk
 ....
 sudo yum install jenkins -y
 systemctl daemon-reload
